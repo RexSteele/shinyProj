880527d82e8ba0b6b6364ce122537b13d6ac5447
Rex Steele <rex@Rexs-MacBook-Air.local>
2020-12-23 17:52:16 -0700

    HTML coloring for changed values

--- app.R
+++ app.R
@@ -79,28 +79,28 @@ ui <- fluidPage(
       #Output for current sequence
       fluidRow(
         column(1,
-               textOutput("p1")
+               htmlOutput("p1")
         ),
         column(1,
-               textOutput("p2")
+               htmlOutput("p2")
         ),
         column(1,
-               textOutput("p3")
+               htmlOutput("p3")
         ),
         column(1,
-               textOutput("p4")
+               htmlOutput("p4")
         ),
         column(1,
-               textOutput("p5")
+               htmlOutput("p5")
         ),
         column(1,
-               textOutput("p6")
+               htmlOutput("p6")
         ),
         column(1,
-               textOutput("p7")
+               htmlOutput("p7")
         ),
         column(1,
-               textOutput("p8")
+               htmlOutput("p8")
         ),
       ),
       #Cryptex Image
@@ -150,6 +150,38 @@ server <- function(input, output) {
 
   source("calcVals.R", local=TRUE)
   source("gameVals.R", local=TRUE)
+
+  #update dataframe
+  updateData <- function(){
+    #add copy of sequence to choices dataframe
+    nRow <- c(gsub("[^A-Z]", "", getCurrPlay1()), gsub("[^A-Z]", "", getCurrPlay2()), gsub("[^A-Z]", "", getCurrPlay3()), gsub("[^A-Z]", "", getCurrPlay4()), gsub("[^A-Z]", "", getCurrPlay5()), gsub("[^A-Z]", "", getCurrPlay6()), gsub("[^A-Z]", "", getCurrPlay7()), gsub("[^A-Z]", "", getCurrPlay8()))
+    choices <<- rbind(choices, nRow)
+  }
+
+  #remove HTML tags from player values
+  flushHTML <- function(){
+    setCurrPlay1(gsub("[^A-Z]", "", getCurrPlay1()))
+    setCurrPlay2(gsub("[^A-Z]", "", getCurrPlay2()))
+    setCurrPlay3(gsub("[^A-Z]", "", getCurrPlay3()))
+    setCurrPlay4(gsub("[^A-Z]", "", getCurrPlay4()))
+    setCurrPlay5(gsub("[^A-Z]", "", getCurrPlay5()))
+    setCurrPlay6(gsub("[^A-Z]", "", getCurrPlay6()))
+    setCurrPlay7(gsub("[^A-Z]", "", getCurrPlay7()))
+    setCurrPlay8(gsub("[^A-Z]", "", getCurrPlay8()))
+  }
+
+  #update player and bind values
+  updateVals <- function(){
+    output$p1 <- renderPrint({HTML(getCurrPlay1())})
+    output$p2 <- renderPrint({HTML(getCurrPlay2())})
+    output$p3 <- renderPrint({HTML(getCurrPlay3())})
+    output$p4 <- renderPrint({HTML(getCurrPlay4())})
+    output$p5 <- renderPrint({HTML(getCurrPlay5())})
+    output$p6 <- renderPrint({HTML(getCurrPlay6())})
+    output$p7 <- renderPrint({HTML(getCurrPlay7())})
+    output$p8 <- renderPrint({HTML(getCurrPlay8())})
+    output$b1 <- renderPrint({HTML(getWholeCurrBind())})
+  }
 
   #updates adist values. Has to be inside server function due to manipulating output object
   adistOut <- function(){
@@ -204,15 +236,7 @@ server <- function(input, output) {
     setWholeOldBind(getWholeCurrBind())
 
     #Output the values
-    output$p1 <- renderText({getCurrPlay1()})
-    output$p2 <- renderText({getCurrPlay2()})
-    output$p3 <- renderText({getCurrPlay3()})
-    output$p4 <- renderText({getCurrPlay4()})
-    output$p5 <- renderText({getCurrPlay5()})
-    output$p6 <- renderText({getCurrPlay6()})
-    output$p7 <- renderText({getCurrPlay7()})
-    output$p8 <- renderText({getCurrPlay8()})
-    output$b1 <- renderText({getWholeCurrBind()})
+    updateVals()
 
     #Reset then output adist values
     output$aDist1 <- renderText({""})
@@ -234,6 +258,8 @@ server <- function(input, output) {
   #Shuffle all button
   observeEvent(input$shuffleAll, {
 
+    flushHTML()
+
     #save current player status
     setWholeOldPlay(getWholeCurrPlay())
     setWholeOldBind(getWholeCurrBind())
@@ -250,58 +276,47 @@ server <- function(input, output) {
 
     #If col is not locked, replace and re-do binding site eval
     if(!l1){
-      setCurrPlay1(createColumn())
-      setCurrBind1(calcBindingSite(getCurrPlay1(), bindTab))
+      setCurrPlay1(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
     }
     if(!l2){
-      setCurrPlay2(createColumn())
-      setCurrBind2(calcBindingSite(getCurrPlay2(), bindTab))
+      setCurrPlay2(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
     }
     if(!l3){
-      setCurrPlay3(createColumn())
-      setCurrBind3(calcBindingSite(getCurrPlay3(), bindTab))
+      setCurrPlay3(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
     }
     if(!l4){
-      setCurrPlay4(createColumn())
-      setCurrBind4(calcBindingSite(getCurrPlay4(), bindTab))
+      setCurrPlay4(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
     }
     if(!l5){
-      setCurrPlay5(createColumn())
-      setCurrBind5(calcBindingSite(getCurrPlay5(), bindTab))
+      setCurrPlay5(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
     }
     if(!l6){
-      setCurrPlay6(createColumn())
-      setCurrBind6(calcBindingSite(getCurrPlay6(), bindTab))
+      setCurrPlay6(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
     }
     if(!l7){
-      setCurrPlay7(createColumn())
-      setCurrBind7(calcBindingSite(getCurrPlay7(), bindTab))
+      setCurrPlay7(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
     }
     if(!l8){
-      setCurrPlay8(createColumn())
-      setCurrBind8(calcBindingSite(getCurrPlay8(), bindTab))
+      setCurrPlay8(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
     }
 
     ##Clean up
-    #add copy of sequence to choices dataframe
-    nRow <- c(getCurrPlay1(), getCurrPlay2(), getCurrPlay3(), getCurrPlay4(), getCurrPlay5(), getCurrPlay6(), getCurrPlay7(), getCurrPlay8())
-    choices <<- rbind(choices, nRow)
+    #Output the values
+    updateVals()
 
-    #Update outputs
-    output$p1 <- renderText({getCurrPlay1()})
-    output$p2 <- renderText({getCurrPlay2()})
-    output$p3 <- renderText({getCurrPlay3()})
-    output$p4 <- renderText({getCurrPlay4()})
-    output$p5 <- renderText({getCurrPlay5()})
-    output$p6 <- renderText({getCurrPlay6()})
-    output$p7 <- renderText({getCurrPlay7()})
-    output$p8 <- renderText({getCurrPlay8()})
-    output$b1 <- renderText({getWholeCurrBind()})
-
-    #Output adist values and increment moveCount
+    #Output adist values, increment moveCount, update dataframe
     addMoveCount()
     moveOut()
     adistOut()
+    updateData()
 
     #Check win condition
     if(checkGoal() != FALSE){
@@ -312,6 +327,8 @@ server <- function(input, output) {
   #Mutation button, shuffles random letter in random column
   observeEvent(input$mutation, {
 
+    flushHTML()
+
     #save current player status
     setWholeOldPlay(getWholeCurrPlay())
     setWholeOldBind(getWholeCurrBind())
@@ -319,58 +336,47 @@ server <- function(input, output) {
     #Pick random col, shuffle single char in that col
     randomCol <- round(runif(1,1,8))
     if(randomCol == 1){
-      setCurrPlay1(oneRandomShuffle(getCurrPlay1()))
-      setCurrBind1(calcBindingSite(getCurrPlay1(), bindTab))
+      setCurrPlay1(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay1()), "</span>"))
+      setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
     }
     if(randomCol == 2){
-      setCurrPlay2(oneRandomShuffle(getCurrPlay2()))
-      setCurrBind2(calcBindingSite(getCurrPlay2(), bindTab))
+      setCurrPlay2(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay2()), "</span>"))
+      setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
     }
     if(randomCol == 3){
-      setCurrPlay3(oneRandomShuffle(getCurrPlay3()))
-      setCurrBind3(calcBindingSite(getCurrPlay3(), bindTab))
+      setCurrPlay3(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay3()), "</span>"))
+      setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
     }
     if(randomCol == 4){
-      setCurrPlay4(oneRandomShuffle(getCurrPlay4()))
-      setCurrBind4(calcBindingSite(getCurrPlay4(), bindTab))
+      setCurrPlay4(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay4()), "</span>"))
+      setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
     }
     if(randomCol == 5){
-      setCurrPlay5(oneRandomShuffle(getCurrPlay5()))
-      setCurrBind5(calcBindingSite(getCurrPlay5(), bindTab))
+      setCurrPlay5(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay5()), "</span>"))
+      setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
     }
     if(randomCol == 6){
-      setCurrPlay6(oneRandomShuffle(getCurrPlay6()))
-      setCurrBind6(calcBindingSite(getCurrPlay6(), bindTab))
+      setCurrPlay6(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay6()), "</span>"))
+      setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
     }
     if(randomCol == 7){
-      setCurrPlay7(oneRandomShuffle(getCurrPlay7()))
-      setCurrBind7(calcBindingSite(getCurrPlay7(), bindTab))
+      setCurrPlay7(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay7()), "</span>"))
+      setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
     }
     if(randomCol == 8){
-      setCurrPlay8(oneRandomShuffle(getCurrPlay8()))
-      setCurrBind8(calcBindingSite(getCurrPlay8(), bindTab))
+      setCurrPlay8(paste("<span style=\"color:red\">", oneRandomShuffle(getCurrPlay8()), "</span>"))
+      setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
     }
 
-    #Clean up
-    #add copy of sequence to choices dataframe
-    nRow <- c(getCurrPlay1(), getCurrPlay2(), getCurrPlay3(), getCurrPlay4(), getCurrPlay5(), getCurrPlay6(), getCurrPlay7(), getCurrPlay8())
-    choices <<- rbind(choices, nRow)
+    ##Clean up
+    #Output the values
+    updateVals()
 
-    #Update outputs
-    output$p1 <- renderText({getCurrPlay1()})
-    output$p2 <- renderText({getCurrPlay2()})
-    output$p3 <- renderText({getCurrPlay3()})
-    output$p4 <- renderText({getCurrPlay4()})
-    output$p5 <- renderText({getCurrPlay5()})
-    output$p6 <- renderText({getCurrPlay6()})
-    output$p7 <- renderText({getCurrPlay7()})
-    output$p8 <- renderText({getCurrPlay8()})
-    output$b1 <- renderText({getWholeCurrBind()})
-
-    #Output adist values
+    #Output adist values, increment moveCount, update dataframe
     addMoveCount()
     moveOut()
     adistOut()
+    updateData()
 
     #Check win condition
     if(checkGoal() != FALSE){
@@ -380,6 +386,8 @@ server <- function(input, output) {
 
   #Recombination button, shuffles random number of contiguous columns
   observeEvent(input$recomb, {
+
+    flushHTML()
 
     #save current player status
     setWholeOldPlay(getWholeCurrPlay())
@@ -391,54 +399,43 @@ server <- function(input, output) {
     endCol <- startCol + colAmount - 1
     markCol <- startCol
 
-    #save reactives as list to new far so it can be iterated
+    #save reactives as list to new var so it can be iterated
     recombVar <- c(getCurrPlay1(), getCurrPlay2(), getCurrPlay3(), getCurrPlay4(), getCurrPlay5(), getCurrPlay6(), getCurrPlay7(), getCurrPlay8())
 
     #iterate new list an modify as appropriate
     for(i in startCol:endCol){
-      recombVar[i] <- createColumn()
-      if(i == 1){setCurrBind1(getCurrPlay1)}
-      if(i == 2){setCurrBind2(getCurrPlay2)}
-      if(i == 3){setCurrBind3(getCurrPlay3)}
-      if(i == 4){setCurrBind4(getCurrPlay4)}
-      if(i == 5){setCurrBind5(getCurrPlay5)}
-      if(i == 6){setCurrBind6(getCurrPlay6)}
-      if(i == 7){setCurrBind7(getCurrPlay7)}
-      if(i == 8){setCurrBind8(getCurrPlay8)}
+      recombVar[i] <- paste("<span style=\"color:red\">", createColumn(), "</span>", sep= "", collapse = "")
+      if(i==1){setCurrPlay1(recombVar[i])}
+      if(i==2){setCurrPlay2(recombVar[i])}
+      if(i==3){setCurrPlay3(recombVar[i])}
+      if(i==4){setCurrPlay4(recombVar[i])}
+      if(i==5){setCurrPlay5(recombVar[i])}
+      if(i==6){setCurrPlay6(recombVar[i])}
+      if(i==7){setCurrPlay7(recombVar[i])}
+      if(i==8){setCurrPlay8(recombVar[i])}
     }
 
-    #re-assign basck to reactive var
-    setWholeCurrPlay(paste(recombVar, sep = "", collapse = ""))
-    setCurrBind1(calcBindingSite(getCurrPlay1(), bindTab))
-    setCurrBind2(calcBindingSite(getCurrPlay2(), bindTab))
-    setCurrBind3(calcBindingSite(getCurrPlay3(), bindTab))
-    setCurrBind4(calcBindingSite(getCurrPlay4(), bindTab))
-    setCurrBind5(calcBindingSite(getCurrPlay5(), bindTab))
-    setCurrBind6(calcBindingSite(getCurrPlay6(), bindTab))
-    setCurrBind7(calcBindingSite(getCurrPlay7(), bindTab))
-    setCurrBind8(calcBindingSite(getCurrPlay8(), bindTab))
+    #re-assign back to vars
+    # setWholeCurrPlay(paste(recombVar, sep = "", collapse = ""))
+    setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
+    setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
+    setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
+    setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
+    setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
+    setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
+    setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
+    setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
 
 
-    #Clean up
-    #add copy of sequence to choices dataframe
-    nRow <- c(getCurrPlay1(), getCurrPlay2(), getCurrPlay3(), getCurrPlay4(), getCurrPlay5(), getCurrPlay6(), getCurrPlay7(), getCurrPlay8())
-    choices <<- rbind(choices, nRow)
+    ##Clean up
+    #Output the values
+    updateVals()
 
-    #Update outputs
-    output$p1 <- renderText({getCurrPlay1()})
-    output$p2 <- renderText({getCurrPlay2()})
-    output$p3 <- renderText({getCurrPlay3()})
-    output$p4 <- renderText({getCurrPlay4()})
-    output$p5 <- renderText({getCurrPlay5()})
-    output$p6 <- renderText({getCurrPlay6()})
-    output$p7 <- renderText({getCurrPlay7()})
-    output$p8 <- renderText({getCurrPlay8()})
-    output$b1 <- renderText({getWholeCurrBind()})
-
-    #Output adist values
+    #Output adist values, increment moveCount, update dataframe
     addMoveCount()
     moveOut()
     adistOut()
+    updateData()
 
     #Check win condition
     if(checkGoal() != FALSE){
@@ -449,64 +446,55 @@ server <- function(input, output) {
   #Shuffle one button
   observeEvent(input$shuffleOneButton, {
 
+    flushHTML()
+
     #save current player status
     setWholeOldPlay(getWholeCurrPlay())
     setWholeOldBind(getWholeCurrBind())
 
     #Shuffle column based on input
     if(input$shuffleOneInput == "Column 1"){
-      setCurrPlay1(createColumn())
-      setCurrBind1(calcBindingSite(getCurrPlay1(), bindTab))
+      setCurrPlay1(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
     }
     if(input$shuffleOneInput == "Column 2"){
-      setCurrPlay2(createColumn())
-      setCurrBind2(calcBindingSite(getCurrPlay2(), bindTab))
+      setCurrPlay2(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
     }
     if(input$shuffleOneInput == "Column 3"){
-      setCurrPlay3(createColumn())
-      setCurrBind3(calcBindingSite(getCurrPlay3(), bindTab))
+      setCurrPlay3(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
     }
     if(input$shuffleOneInput == "Column 4"){
-      setCurrPlay4(createColumn())
-      setCurrBind4(calcBindingSite(getCurrPlay4(), bindTab))
+      setCurrPlay4(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
     }
     if(input$shuffleOneInput == "Column 5"){
-      setCurrPlay5(createColumn())
-      setCurrBind5(calcBindingSite(getCurrPlay5(), bindTab))
+      setCurrPlay5(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
     }
     if(input$shuffleOneInput == "Column 6"){
-      setCurrPlay6(createColumn())
-      setCurrBind6(calcBindingSite(getCurrPlay6(), bindTab))
+      setCurrPlay6(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
     }
     if(input$shuffleOneInput == "Column 7"){
-      setCurrPlay7(createColumn())
-      setCurrBind7(calcBindingSite(getCurrPlay7(), bindTab))
+      setCurrPlay7(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
     }
     if(input$shuffleOneInput == "Column 8"){
-      setCurrPlay8(createColumn())
-      setCurrBind8(calcBindingSite(getCurrPlay8(), bindTab))
+      setCurrPlay8(paste("<span style=\"color:red\">", createColumn(), "</span>"))
+      setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
     }
 
-    #Clean up
-    #add copy of sequence to choices dataframe
-    nRow <- c(getCurrPlay1(), getCurrPlay2(), getCurrPlay3(), getCurrPlay4(), getCurrPlay5(), getCurrPlay6(), getCurrPlay7(), getCurrPlay8())
-    choices <<- rbind(choices, nRow)
+    ##Clean up
+    #Output the values
+    updateVals()
 
-    #Update outputs
-    output$p1 <- renderText({getCurrPlay1()})
-    output$p2 <- renderText({getCurrPlay2()})
-    output$p3 <- renderText({getCurrPlay3()})
-    output$p4 <- renderText({getCurrPlay4()})
-    output$p5 <- renderText({getCurrPlay5()})
-    output$p6 <- renderText({getCurrPlay6()})
-    output$p7 <- renderText({getCurrPlay7()})
-    output$p8 <- renderText({getCurrPlay8()})
-    output$b1 <- renderText({getWholeCurrBind()})
-
-    #Output adist values
+    #Output adist values, increment moveCount, update dataframe
     addMoveCount()
-    output$moveCount <- renderText({getMoveCount()})
+    moveOut()
     adistOut()
+    updateData()
 
     #Check win condition
     if(checkGoal() != FALSE){
@@ -525,16 +513,8 @@ server <- function(input, output) {
     nRow <- c("^^", "^^", "RE", "VE", "RT", "ED", "^^", "^^")
     choices <<- rbind(choices, nRow)
 
-    #Update outputs
-    output$p1 <- renderText({getCurrPlay1()})
-    output$p2 <- renderText({getCurrPlay2()})
-    output$p3 <- renderText({getCurrPlay3()})
-    output$p4 <- renderText({getCurrPlay4()})
-    output$p5 <- renderText({getCurrPlay5()})
-    output$p6 <- renderText({getCurrPlay6()})
-    output$p7 <- renderText({getCurrPlay7()})
-    output$p8 <- renderText({getCurrPlay8()})
-    output$b1 <- renderText({getWholeCurrBind()})
+    #Output the values
+    updateVals()
 
     #Output adist values
     subMoveCount()
