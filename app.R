f10f1f958d352149eb465a2841f6be546e6976d1
Rex Steele <rex@Rexs-MacBook-Air.local>
2021-01-14 23:48:33 -0700

    fixes to variable structure

--- app.R
+++ app.R
@@ -171,42 +171,37 @@ server <- function(input, output) {
 
   #remove HTML tags from player values
   flushHTML <- function(){
-    setCurrPlay1(gsub("[^A-Z]", "", getCurrPlay1()))
-    setCurrPlay2(gsub("[^A-Z]", "", getCurrPlay2()))
-    setCurrPlay3(gsub("[^A-Z]", "", getCurrPlay3()))
-    setCurrPlay4(gsub("[^A-Z]", "", getCurrPlay4()))
-    setCurrPlay5(gsub("[^A-Z]", "", getCurrPlay5()))
-    setCurrPlay6(gsub("[^A-Z]", "", getCurrPlay6()))
-    setCurrPlay7(gsub("[^A-Z]", "", getCurrPlay7()))
-    setCurrPlay8(gsub("[^A-Z]", "", getCurrPlay8()))
+    for(x in 1:8){
+      setCurrPlay(gsub("[^A-Z]", "", getCurrPlay(x)), x)
+    }
   }
 
   #update player and bind values
   updateVals <- function(){
-    output$p1 <- renderPrint({HTML(getCurrPlay1())})
-    output$p2 <- renderPrint({HTML(getCurrPlay2())})
-    output$p3 <- renderPrint({HTML(getCurrPlay3())})
-    output$p4 <- renderPrint({HTML(getCurrPlay4())})
-    output$p5 <- renderPrint({HTML(getCurrPlay5())})
-    output$p6 <- renderPrint({HTML(getCurrPlay6())})
-    output$p7 <- renderPrint({HTML(getCurrPlay7())})
-    output$p8 <- renderPrint({HTML(getCurrPlay8())})
+    output$p1 <- renderPrint({HTML(getCurrPlay(1))})
+    output$p2 <- renderPrint({HTML(getCurrPlay(2))})
+    output$p3 <- renderPrint({HTML(getCurrPlay(3))})
+    output$p4 <- renderPrint({HTML(getCurrPlay(4))})
+    output$p5 <- renderPrint({HTML(getCurrPlay(5))})
+    output$p6 <- renderPrint({HTML(getCurrPlay(6))})
+    output$p7 <- renderPrint({HTML(getCurrPlay(7))})
+    output$p8 <- renderPrint({HTML(getCurrPlay(8))})
     output$b1 <- renderPrint({HTML(getWholeCurrBind())})
   }
 
   #updates adist values. Has to be inside server function due to manipulating output object
   adistOut <- function(){
     output$aDist1 <- renderText({paste("Secret Key 1 score: ", calcAdist(1))})
-    if(!is.na(getGoalBind()[2])){
+    if(!is.na(getGoalArr()[2])){
       output$aDist2 <- renderText({paste("Secret Key 2 score: ",calcAdist(2))})
     }
-    if(!is.na(getGoalBind()[3])){
+    if(!is.na(getGoalArr()[3])){
       output$aDist3 <- renderText({paste("Secret Key 3 score: ",calcAdist(3))})
     }
-    if(!is.na(getGoalBind()[4])){
+    if(!is.na(getGoalArr()[4])){
       output$aDist4 <- renderText({paste("Secret Key 4 score: ",calcAdist(4))})
     }
-    if(!is.na(getGoalBind()[5])){
+    if(!is.na(getGoalArr()[5])){
       output$aDist5 <- renderText({paste("Secret Key 5 score: ",calcAdist(5))})
     }
   }
@@ -225,22 +220,10 @@ server <- function(input, output) {
     removeModal()
 
     #set player vals
-    setCurrPlay1(createColumn())
-    setCurrBind1(calcBindingSite(getCurrPlay1(), bindTab))
-    setCurrPlay2(createColumn())
-    setCurrBind2(calcBindingSite(getCurrPlay2(), bindTab))
-    setCurrPlay3(createColumn())
-    setCurrBind3(calcBindingSite(getCurrPlay3(), bindTab))
-    setCurrPlay4(createColumn())
-    setCurrBind4(calcBindingSite(getCurrPlay4(), bindTab))
-    setCurrPlay5(createColumn())
-    setCurrBind5(calcBindingSite(getCurrPlay5(), bindTab))
-    setCurrPlay6(createColumn())
-    setCurrBind6(calcBindingSite(getCurrPlay6(), bindTab))
-    setCurrPlay7(createColumn())
-    setCurrBind7(calcBindingSite(getCurrPlay7(), bindTab))
-    setCurrPlay8(createColumn())
-    setCurrBind8(calcBindingSite(getCurrPlay8(), bindTab))
+    for(x in 1:8){
+      setCurrPlay(createColumn(), x)
+      setCurrBind(calcBindingSite(getCurrPlay(x), bindTab))
+    }
 
     #set old vals to current vals for first iteration
     setWholeOldPlay(getWholeCurrPlay())
@@ -275,48 +258,12 @@ server <- function(input, output) {
     setWholeOldPlay(getWholeCurrPlay())
     setWholeOldBind(getWholeCurrBind())
 
-    #Check which cols are locked, assigns boolean
-    l1 <- "lockCol1" %in% input$lockCheck
-    l2 <- "lockCol2" %in% input$lockCheck
-    l3 <- "lockCol3" %in% input$lockCheck
-    l4 <- "lockCol4" %in% input$lockCheck
-    l5 <- "lockCol5" %in% input$lockCheck
-    l6 <- "lockCol6" %in% input$lockCheck
-    l7 <- "lockCol7" %in% input$lockCheck
-    l8 <- "lockCol8" %in% input$lockCheck
-
     #If col is not locked, replace and re-do binding site eval
-    if(!l1){
-      setCurrPlay1(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
-    }
-    if(!l2){
-      setCurrPlay2(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
-    }
-    if(!l3){
-      setCurrPlay3(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
-    }
-    if(!l4){
-      setCurrPlay4(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
-    }
-    if(!l5){
-      setCurrPlay5(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
-    }
-    if(!l6){
-      setCurrPlay6(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
-    }
-    if(!l7){
-      setCurrPlay7(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
-    }
-    if(!l8){
-      setCurrPlay8(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
+    for(x in 1:8){
+      if(!(paste("lockCol", toString(x), sep="") %in% input$lockCheck)){
+        setCurrPlay(paste("<span style=\"color:red\">", createColumn(), "</span>"), x)
+        setCurrBind(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay(x)), bindTab), x)
+      }
     }
 
     ##Clean up
@@ -347,38 +294,8 @@ server <- function(input, output) {
 
     #Pick random col, shuffle single char in that col
     randomCol <- round(runif(1,1,8))
-    if(randomCol == 1){
-      setCurrPlay1(oneRandomShuffle(getCurrPlay1()))
-      setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
-    }
-    if(randomCol == 2){
-      setCurrPlay2(oneRandomShuffle(getCurrPlay2()))
-      setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
-    }
-    if(randomCol == 3){
-      setCurrPlay3(oneRandomShuffle(getCurrPlay3()))
-      setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
-    }
-    if(randomCol == 4){
-      setCurrPlay4(oneRandomShuffle(getCurrPlay4()))
-      setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
-    }
-    if(randomCol == 5){
-      setCurrPlay5(oneRandomShuffle(getCurrPlay5()))
-      setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
-    }
-    if(randomCol == 6){
-      setCurrPlay6(oneRandomShuffle(getCurrPlay6()))
-      setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
-    }
-    if(randomCol == 7){
-      setCurrPlay7(oneRandomShuffle(getCurrPlay7()))
-      setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
-    }
-    if(randomCol == 8){
-      setCurrPlay8(oneRandomShuffle(getCurrPlay8()))
-      setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
-    }
+    setCurrPlay(oneRandomShuffle(getCurrPlay(randomCol)), randomCol)
+    setCurrBind(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay(randomCol)), bindTab), randomCol)
 
     ##Clean up
     #Output the values
@@ -412,33 +329,10 @@ server <- function(input, output) {
     endCol <- startCol + colAmount - 1
     markCol <- startCol
 
-    #save reactives as list to new var so it can be iterated
-    recombVar <- c(getCurrPlay1(), getCurrPlay2(), getCurrPlay3(), getCurrPlay4(), getCurrPlay5(), getCurrPlay6(), getCurrPlay7(), getCurrPlay8())
-
-    #iterate new list an modify as appropriate
-    for(i in startCol:endCol){
-      recombVar[i] <- paste("<span style=\"color:red\">", createColumn(), "</span>", sep= "", collapse = "")
-      if(i==1){setCurrPlay1(recombVar[i])}
-      if(i==2){setCurrPlay2(recombVar[i])}
-      if(i==3){setCurrPlay3(recombVar[i])}
-      if(i==4){setCurrPlay4(recombVar[i])}
-      if(i==5){setCurrPlay5(recombVar[i])}
-      if(i==6){setCurrPlay6(recombVar[i])}
-      if(i==7){setCurrPlay7(recombVar[i])}
-      if(i==8){setCurrPlay8(recombVar[i])}
+    for(x in startCol:endCol){
+      setCurrPlay(paste("<span style=\"color:red\">", createColumn(), "</span>", sep= "", collapse = ""), x)
+      setCurrBind(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay(x)), bindTab), x)
     }
-
-    #re-assign back to vars
-    # setWholeCurrPlay(paste(recombVar, sep = "", collapse = ""))
-    setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
-    setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
-    setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
-    setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
-    setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
-    setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
-    setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
-    setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
-
 
     ##Clean up
     #Output the values
@@ -467,37 +361,11 @@ server <- function(input, output) {
     setWholeOldBind(getWholeCurrBind())
 
     #Shuffle column based on input
-    if(input$shuffleOneInput == "Column 1"){
-      setCurrPlay1(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind1(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay1()), bindTab))
-    }
-    if(input$shuffleOneInput == "Column 2"){
-      setCurrPlay2(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind2(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay2()), bindTab))
-    }
-    if(input$shuffleOneInput == "Column 3"){
-      setCurrPlay3(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind3(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay3()), bindTab))
-    }
-    if(input$shuffleOneInput == "Column 4"){
-      setCurrPlay4(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind4(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay4()), bindTab))
-    }
-    if(input$shuffleOneInput == "Column 5"){
-      setCurrPlay5(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind5(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay5()), bindTab))
-    }
-    if(input$shuffleOneInput == "Column 6"){
-      setCurrPlay6(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind6(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay6()), bindTab))
-    }
-    if(input$shuffleOneInput == "Column 7"){
-      setCurrPlay7(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind7(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay7()), bindTab))
-    }
-    if(input$shuffleOneInput == "Column 8"){
-      setCurrPlay8(paste("<span style=\"color:red\">", createColumn(), "</span>"))
-      setCurrBind8(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay8()), bindTab))
+    for(x in 1:8){
+      if(paste("Column", toString(x), sep=" ") %in% input$shuffleOneInput){
+        setCurrPlay(paste("<span style=\"color:red\">", createColumn(), "</span>"), x)
+        setCurrBind(calcBindingSite(gsub("[^A-Z]", "", getCurrPlay(x)), bindTab), x)
+      }
     }
 
     ##Clean up
@@ -537,6 +405,7 @@ server <- function(input, output) {
     adistOut()
   })
 
+  #Auto Solve functionality #####TO DO DOES NOT WORK AS EXPECTED#####
   observeEvent(input$autoSolve, {
     while(checkGoal() == FALSE){
       if(calcAdist(1) >= 5){
